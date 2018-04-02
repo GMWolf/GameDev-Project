1b37694f780d224da7b43f3e61264a71ab718179
Felix Bridault <b5023110@newcastle.ac.uk>
2018-04-02 16:07:31 +0200

    Load Scene added

---
+++ ECS/ECS.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include "json.hpp"
+#include <fstream>
+#include "Entity.h"
+
+inline void loadScene(std::string fileName)
+{
+
+		std::ifstream i(fileName);
+		nlohmann::json j;
+		i >> j;
+
+		for (auto& jentity : j)
+		{
+			Entity::create(jentity);
+		}
+}
